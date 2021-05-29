33d7bfdcc54c4397bd8f562a64240d29afb9091c
Adrian Galindo <asdrian4455@hotmail.com>
2021-05-28 23:47:56 -0500

    lastcommit

--- src/main/java/com/zuta/adminpanel/AdminpanelApplication.java
+++ src/main/java/com/zuta/adminpanel/AdminpanelApplication.java
@@ -1,13 +1,35 @@
 package com.zuta.adminpanel;
 
+import com.zuta.adminpanel.domain.tipoTapa;
+import com.zuta.adminpanel.infrastructure.repository.jpa.TipoTapasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminpanelApplication {
+public class AdminpanelApplication implements CommandLineRunner {
+
+
+    @Autowired
+    private TipoTapasRepository tipoTapasRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(AdminpanelApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        tipoTapa tipo = new tipoTapa();
+        tipo.setTipoTapa("corchos");
+        tipoTapa tipo2 = new tipoTapa();
+        tipo.setTipoTapa("chapas");
+
+        this.tipoTapasRepository.save(tipo);
+        this.tipoTapasRepository.save(tipo2);
+
+    }
 }
